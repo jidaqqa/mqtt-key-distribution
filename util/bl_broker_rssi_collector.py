49aae13078588745042e76bc2e9c5638b9c112ed
jidaqqa <jihadmdaqqa@gmail.com>
2022-07-22 11:58:08 +0200

    Adding Data Collector for LORA WAN and BL

---
+++ util/bl_broker_rssi_collector.py
@@ -0,0 +1,168 @@
+import logging
+import subprocess
+
+from bluetooth import *  # Python Bluetooth library
+
+
+class bleServer:
+    def __init__(self, serverSocket=None, clientSocket=None):
+        if serverSocket is None:
+            self.serverSocket = serverSocket
+            self.clientSocket = clientSocket
+            self.serviceName = "MQTT-Broker"
+            self.uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+        else:
+            self.serverSocket = serverSocket
+            self.clientSocket = clientSocket
+
+    def getBluetoothSocket(self):
+        try:
+            self.serverSocket = BluetoothSocket(RFCOMM)
+            logging.info("Bluetooth server socket successfully created for RFCOMM service...")
+        except (BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to create the bluetooth server socket ", exc_info=True)
+
+    def getBluetoothConnection(self):
+        try:
+            self.serverSocket.bind(("", PORT_ANY))
+            logging.info("Bluetooth server socket bind successfully on host "" to PORT_ANY...")
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to bind server socket on host to PORT_ANY ... ", exc_info=True)
+        try:
+            self.serverSocket.listen(1)
+            logging.info("Bluetooth server socket put to listening mode successfully ...")
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to put server socket to listening mode  ... ", exc_info=True)
+        try:
+            port = self.serverSocket.getsockname()[1]
+            logging.info("Waiting for connection on RFCOMM channel %d" % (port))
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to get connection on RFCOMM channel  ... ", exc_info=True)
+
+    def advertiseBluetoothService(self):
+        try:
+            advertise_service(self.serverSocket, self.serviceName,
+                              service_id=self.uuid,
+                              service_classes=[self.uuid, SERIAL_PORT_CLASS],
+                              profiles=[SERIAL_PORT_PROFILE],
+                              #                   protocols = [ OBEX_UUID ]
+                              )
+            logging.info("%s advertised successfully ..." % (self.serviceName))
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to advertise bluetooth services  ... ", exc_info=True)
+
+    def acceptBluetoothConnection(self):
+        try:
+            self.clientSocket, clientInfo = self.serverSocket.accept()
+            logging.info("Accepted bluetooth connection from %s", clientInfo)
+            return clientInfo
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to accept bluetooth connection ... ", exc_info=True)
+
+    def recvData(self):
+        try:
+            while True:
+                data = self.clientSocket.recv(1024)
+                if not data:
+                    break
+                return data.decode()
+            logging.info("Data received successfully over bluetooth connection")
+            return data
+        except (Exception, IOError, BluetoothError) as e:
+            pass
+
+    def closeBluetoothSocket(self):
+        try:
+            if self.clientSocket:
+                self.clientSocket.close()
+            self.serverSocket.close()
+            logging.info("Bluetooth sockets successfully closed ...")
+        except (Exception, BluetoothError) as e:
+            logging.error("Failed to close the bluetooth sockets ", exc_info=True)
+
+    def closeClientSocket(self):
+        try:
+            self.clientSocket.close()
+            logging.info("Client Bluetooth sockets successfully closed ...")
+        except (Exception, BluetoothError) as e:
+            logging.error("Failed to close the bluetooth sockets ", exc_info=True)
+
+    def sendData(self, data):
+        logging.info("Sending Data!")
+        self.clientSocket.send(data)
+
+    def start(self):
+        # Create the server socket
+        self.getBluetoothSocket()
+        # get bluetooth connection to port # of the first available
+        self.getBluetoothConnection()
+        # advertising bluetooth services
+        self.advertiseBluetoothService()
+        # Accepting bluetooth connection
+        #self.acceptBluetoothConnection()
+
+    def receive(self):
+        # receive data
+        dataRecv = self.recvData()
+        logging.info(dataRecv)
+        return dataRecv
+
+    def stop(self):
+        # Disconnecting bluetooth sockets
+        self.closeBluetoothSocket()
+
+    def get_rssi(self, addr):
+        try:
+            p = subprocess.Popen('sudo btmgmt find | grep {}'.format(addr), stdout=subprocess.PIPE, shell=True)
+            a, b = p.communicate()
+            read_lst = []
+            for reading in str(a).split('\\n')[:-1]:
+                reading = reading.split('rssi ')[1]
+                reading = reading.split(' flags')[0]
+                reading = int(reading)
+                read_lst.append(reading)
+            if len(read_lst) > 0:
+                return read_lst[0]
+            else:
+                logging.info("Make sure your device is discoverable, disconnect and connect again!")
+                return None
+        except SystemError as e:
+            logging.info("Make sure your device is discoverable, disconnect and connect again!")
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s',
+                                  '%m-%d-%Y %H:%M:%S')
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler('logs.log')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stdout_handler)
+
+    bleSvr = bleServer()
+    bleSvr.start()
+    clientInfo = bleSvr.acceptBluetoothConnection()
+
+    count = 0
+    while True:
+        current_rssi = bleSvr.get_rssi(clientInfo[0])
+        if current_rssi is not None:
+            print(f"Current RSSI: {str(current_rssi)}")
+            with open('bl.txt', 'a') as f:
+                f.write(str(current_rssi))
+                f.write("\n")
+            count = count + 1
+            if count == 20:
+                bleSvr.start()
+                break
+
+    # bleSvr.receive()
+    # bleSvr.stop()
---
+++ util/bl_client_rssi_collector.py
@@ -0,0 +1,127 @@
+import logging
+import logging.config
+from bluetooth import *  # Python Bluetooth library
+
+
+class bleClient:
+    def __init__(self, clientSocket=None):
+        if clientSocket is None:
+            self.clientSocket = clientSocket
+            self.bleService = None
+            self.addr = None
+            self.uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+        else:
+            self.clientSocket = clientSocket
+
+    def getBluetoothServices(self):
+        try:
+            bleService = {}
+            logging.info("Searching for  Bluetooth services ...")
+            for reConnect in range(1, 4):
+                bleService = find_service(name="MQTT-Broker", address=self.addr)
+                if len(bleService) == 0:
+                    logging.info("Re-connecting  Bluetooth services : %d attempt", reConnect)
+                else:
+                    break
+            if not bleService:
+                raise SystemExit()
+            else:
+                logging.info("Found  Bluetooth services ..")
+                logging.info("Protocol\t: %s", bleService[0]['protocol'])
+                logging.info("Name\t\t: %s", bleService[0]['name'])
+                logging.info("Service-id\t: %s", bleService[0]['service-id'])
+                logging.info("Profiles\t: %s", bleService[0]['profiles'])
+                logging.info("Service-class\t: %s", bleService[0]['service-classes'])
+                logging.info("Host\t\t: %s", bleService[0]['host'])
+                logging.info("Provider\t: %s", bleService[0]['provider'])
+                logging.info("Port\t\t: %s", bleService[0]['port'])
+                logging.info("Description\t: %s", bleService[0]['description'])
+                self.bleService = bleService
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Couldn't find the MQTT-Broker Bluetooth service", exc_info=True)
+
+    def getBluetoothSocket(self):
+        try:
+            self.clientSocket = BluetoothSocket(RFCOMM)
+            logging.info("Bluetooth client socket successfully created for RFCOMM service  ...")
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to create the bluetooth client socket for RFCOMM service  ...  ", exc_info=True)
+
+    def getBluetoothConnection(self):
+        bleServiceInfo = {}
+        try:
+            bleServiceInfo = self.bleService[0]
+            logging.info("Connecting to \"%s\" on %s with port %s" % (
+                bleServiceInfo['name'], bleServiceInfo['host'], bleServiceInfo['port']))
+            self.clientSocket.connect((bleServiceInfo['host'], bleServiceInfo['port']))
+            logging.info("Connected successfully to %s " % (bleServiceInfo['name']))
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to connect to \"%s\" on address %s with port %s" % (
+                bleServiceInfo['name'], bleServiceInfo['host'], bleServiceInfo['port']), exc_info=True)
+
+    def sendData(self, msg):
+        logging.info("Sending Data!")
+        self.clientSocket.send(msg)
+
+    def recvData(self):
+        try:
+            while True:
+                data = self.clientSocket.recv(1024)
+                if not data:
+                    break
+                return data.decode()
+            logging.info("Data received successfully over bluetooth connection")
+            return data
+        except (Exception, IOError, BluetoothError) as e:
+            pass
+
+    def closeBluetoothSocket(self):
+        try:
+            self.clientSocket.close()
+            logging.info("Bluetooth client socket successfully closed ...")
+        except (Exception, BluetoothError, SystemExit, KeyboardInterrupt) as e:
+            logging.error("Failed to close the bluetooth client socket ", exc_info=True)
+
+    def start(self, msg):
+        # Search for the MQTT-Broker Bluetooth service
+        self.getBluetoothServices()
+        # Create the client socket
+        self.getBluetoothSocket()
+        # Connect to bluetooth service
+        self.getBluetoothConnection()
+        # Send the identifier that request a key
+        self.sendData(msg)
+
+    def receive(self):
+        # receive data
+        dataRecv = self.recvData()
+        logging.info(dataRecv)
+        return dataRecv
+
+    def stop(self):
+        # Disconnecting bluetooth service
+        self.closeBluetoothSocket()
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s',
+                                  '%m-%d-%Y %H:%M:%S')
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler('logs.log')
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stdout_handler)
+
+    logging.info("Setup logging configuration")
+    bleClnt = bleClient()
+    bleClnt.start("0")
+    # bleClnt.receive()
+    # bleClnt.stop()
